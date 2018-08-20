fb1b5fa7c839346b85924d07222c48340d3a5264
Spencer Smith <stix1919@hotmail.com>
2018-08-19 22:08:07 -0500

    More notes in hero reducer

--- src/ducks/heroReducer.js
+++ src/ducks/heroReducer.js
@@ -65,6 +65,8 @@ export function getWeaponExp(weapon, abilities) {
     }
 }
 
+
+//resets exp and adds to level
 export function levelUp(exp, level, nextLevel, hero){
     
     let newLevel = level += 1
